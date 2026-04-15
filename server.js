f86984af7b6559c2da4cef383b20fe8b5bf260e4
Mateus Arruda <arrudamateus180@gmail.com>
2026-04-14 21:52:56 -0300

    fix: usar require.main.filename para resolver caminho na Vercel

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const path = require("path");
 const { URL } = require("url");
 
-const ROOT = process.cwd();
+const ROOT = path.dirname(require.main.filename);
 const PORT = Number(process.env.PORT || 3010);
 
 loadEnvFile(path.join(ROOT, ".env.local"));
