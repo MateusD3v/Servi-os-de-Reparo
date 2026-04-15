d799cd9abd94ce971d8bb32a6e5dca232da7aeb1
Mateus Arruda <arrudamateus180@gmail.com>
2026-04-14 21:50:02 -0300

    fix: usar process.cwd() para resolucao de caminhos na Vercel

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const path = require("path");
 const { URL } = require("url");
 
-const ROOT = __dirname;
+const ROOT = process.cwd();
 const PORT = Number(process.env.PORT || 3010);
 
 loadEnvFile(path.join(ROOT, ".env.local"));
